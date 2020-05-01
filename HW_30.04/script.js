20f607f86ae991cb584dc0dce31162c8b99f5224
bazhena-matyuk <61617313+bazhena-matyuk@users.noreply.github.com>
2020-05-01 21:31:47 +0300

    Add files via upload

---
+++ HW_30.04/script.js
@@ -0,0 +1,79 @@
+let data = [
+    {
+        firstName: 'Ashton',
+        lastName: 'Kutcher',
+        age: 40
+    }, {
+        firstName: 'Bradley',
+        lastName: 'Pitt',
+        age: 54
+    }, {
+        firstName: 'Hannah',
+        lastName: 'Dakota',
+        age: 24
+    }
+];
+
+const addUserBtn = document.querySelector('.add_user_btn');
+addUserBtn.addEventListener('click', addUser);
+
+const deleteUserBtn = document.querySelector('.delete_user_btn');
+deleteUserBtn.addEventListener('click', deleteUser);
+
+const editUserBtn = document.querySelector('.edit_user_btn');
+editUserBtn.addEventListener('click', editUser);
+
+let tbody = document.querySelector('table tbody');
+
+for(let i = 0; i < data.length; i++) {
+    let tr = document.createElement('tr');
+
+    for(let key in data[i]) {
+        let td = document.createElement('td');
+        td.innerHTML = data[i][key];
+        tr.append(td);
+    }
+
+    tbody.append(tr);
+}
+
+function addUser() {
+    let firstName = prompt('input first name', 'Vasya');
+    let lastName = prompt('input last name', 'Pypkin');
+    let age = prompt('input age', 40);
+    let tr = document.createElement('tr');
+    tr.innerHTML = `            
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${age}</td>`;
+    tbody.append(tr);
+}
+
+function deleteUser() {
+    let deleteUserData = prompt('input first name or last name or age');
+    tdArr = tbody.querySelectorAll('td');
+    for(let i = 0; i < tdArr.length; i++) {
+        if (deleteUserData === tdArr[i].innerHTML) {
+                let tr = tdArr[i].closest('tr');
+                tr.remove();
+        }
+    }
+}
+
+function editUser() {
+    let editUserData = prompt('input name or last name or age');
+    let firstName = prompt('input first name', 'Vasya');
+    let lastName = prompt('input last name', 'Pypkin');
+    let age = prompt('input age', 40);
+    tdArr = tbody.querySelectorAll('td');
+    for(let i = 0; i < tdArr.length; i++) {
+        if (editUserData === tdArr[i].innerHTML) {
+            let tr = tdArr[i].closest('tr');
+            tr.innerHTML = `            
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${age}</td>`;
+            tbody.replaseWith(tr);
+        }
+    }
+}
